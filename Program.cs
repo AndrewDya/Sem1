0f55addacc15e2437bbc12a98a634f7bb78cc78d
Andrew <andrewdya@gmail.com>
2023-01-28 14:17:24 +0600

    Задачи

--- Program.cs
+++ Program.cs
@@ -1,17 +1,4 @@
-﻿
-// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
-// [1,0,1,1,0,1,0,0]
-
-int [] numbers = new int[8];
-Console.Write("[");
-for (int i = 0; i < numbers.Length; i++)
- {
-    numbers [i] = new Random().Next(0, 2);
-    Console.Write($"{numbers [i]}, ");
- }
-Console.Write("]");
-
-// Теория
+﻿// Теория
 // Push an existing repository from the command line
 // git remote add origin https://github.com/AndrewDya/Sem1.git
 // git branch -M master
